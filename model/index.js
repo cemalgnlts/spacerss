6b7f0114d591a1283b6b1e55d2613ec3512b1cf6
Cemal Gönültaş <cemalgnlts@gmail.com>
2023-03-20 14:00:09 +0300

    Update deta base

--- model/index.js
+++ model/index.js
@@ -1,7 +1,6 @@
 import { Deta } from "deta";
 
-// const deta = Deta("a057dzwLiMAZ_9hHwQ51Ko8YnB97Anc68zj4iLXR79DWD");
-const deta = Deta(process.env.BASE_KEY);
+const deta = process.env.BASE_KEY ? Deta(process.env.BASE_KEY) : Deta();
 
 const Base = name => deta.Base(name);
 export default Base;
